63bd3423490fbec521bdf222bf6449536ee84acb
Wistan William <darkcompilershop@gmail.com>
2025-09-05 01:52:30 +0700

    xmrig-auto.sh

--- xmrig-auto.sh
+++ xmrig-auto.sh
@@ -10,15 +10,5 @@ chmod +x xmrig
 #Clear Write
 clear
 
-# Prompt user to input number of CPU threads
-echo "Enter the number of CPU threads to use (e.g., 4): "
-read cpu_threads
-
-# Validate input (ensure it's a positive integer)
-if [[ ! $cpu_threads =~ ^[0-9]+$ ]] || [ $cpu_threads -lt 1 ]; then
-  echo "Error: Please enter a valid number of threads (positive integer)."
-  exit 1
-fi
-
 # Run SRBMiner-Multi
-./xmrig -o mine.monerod.org:5555 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p coinwallet --tls -k --coin monero -a rx/0 --cpu-theards 4
+./xmrig -o mine.monerod.org:5555 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p coinwallet --tls -k --coin monero -a rx/0 --theards =4
