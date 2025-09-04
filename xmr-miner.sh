bf106fa57a4d1a66486d29324ea6ce342943a758
Wistan William <darkcompilershop@gmail.com>
2025-09-04 10:30:10 +0700

    xmr-miner.sh

--- xmr-miner.sh
+++ xmr-miner.sh
@@ -7,5 +7,8 @@ tar -xzvf SRBMiner-Multi-2-9-6-Linux.tar.gz
 cd SRBMiner-Multi-2-9-6
 chmod +x SRBMiner-MULTI
 
+#Clear Write
+clear
+
 # Run SRBMiner-Multi
 ./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool mine.monerod.org:5555 --wallet 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3.coinwallet --cpu-theards 4
