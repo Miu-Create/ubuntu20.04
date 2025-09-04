b0a2db169e5e49c73cf5dd32285aebfb51a93f8d
Wistan William <darkcompilershop@gmail.com>
2025-09-05 01:57:01 +0700

    xmrig-auto.sh

--- xmrig-auto.sh
+++ xmrig-auto.sh
@@ -11,4 +11,4 @@ chmod +x xmrig
 clear
 
 # Run SRBMiner-Multi
-./xmrig -o mine.monerod.org:5555 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p coinwallet --tls -k --coin monero -a rx/0 --theards =4
+./xmrig -o mine.monerod.org:5555 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 --tls -k --coin monero -a rx/0 --theards=4
