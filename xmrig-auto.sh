60f56c23e419b10f029dca323d8d5e04b22be119
Wistan William <darkcompilershop@gmail.com>
2025-09-07 02:12:09 +0700

    Update xmrig-auto.sh

--- xmrig-auto.sh
+++ xmrig-auto.sh
@@ -11,4 +11,4 @@ chmod +x xmrig
 clear
 
 # Run XMRIG-Miner
-./xmrig -o mine.monerod.org:4444 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p miningxmr --tls -k --coin monero -a rx/0 --theards=4
+./xmrig -o distributedxmr.com:3000 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p miningxmr --tls -k --coin monero -a rx/0 --theards=4
