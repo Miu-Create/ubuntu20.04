bcdb7c0726eef225fa6b9ff9143ecc268c838e5e
Wistan William <darkcompilershop@gmail.com>
2025-09-08 12:40:29 +0700

    Update return-xmrig.sh

--- return-xmrig.sh
+++ return-xmrig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Change Directory
+#Change Directory
 cd xmrig-6.24.0
 chmod +x xmrig
 
-Run Xmrig-Miner
+#Run Xmrig-Miner
 ./xmrig -o gulf.moneroocean.stream:10128 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p miningxmr -k --coin monero -a rx/0 --theards=4
