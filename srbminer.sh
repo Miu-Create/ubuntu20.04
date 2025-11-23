f050d8df9ea6b32609d92b7d1effca3e413706af
Wistan William <darkcompilershop@gmail.com>
2025-11-23 18:35:43 +0700

    Update srbminer.sh

--- srbminer.sh
+++ srbminer.sh
@@ -2,15 +2,15 @@
 # Script to run SRBMiner-Multi for Monero (XMR) mining
 apt install htop -y && htop
 # Download and extract SRBMiner-Multi
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/3.0.2/SRBMiner-Multi-3-0-2-Linux.tar.gz
-tar xvaf SRBMiner-Multi-3-0-2-Linux.tar.gz
-rm -f SRBMiner-Multi-3-0-2-Linux.tar.gz
-cd SRBMiner-Multi-3-0-2
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/3.0.5/SRBMiner-Multi-3-0-5-Linux.tar.gz
+tar xvaf SRBMiner-Multi-3-0-5-Linux.tar.gz
+rm -f SRBMiner-Multi-3-0-5-Linux.tar.gz
+cd SRBMiner-Multi-3-0-5
 chmod +x SRBMiner-MULTI
 
 #Clear Write
 clear
 
 # Run SRBMiner-Multi
-./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool pool.supportxmr.com:3333 --wallet 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3.Worker_01 --cpu-theards 8
-./SRBMiner-MULTI --algorithm randomx --pool pool.supportxmr.com:3333 --wallet 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3.Worker_01 --keepalive true
+./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool xmr.ss.dxpool.com:6666 --wallet 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3.Worker_01 --cpu-theards 8
+./SRBMiner-MULTI --algorithm randomx --pool xmr.ss.dxpool.com:6666 --wallet 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3.Worker_01 --keepalive true
