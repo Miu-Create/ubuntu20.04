4678e548ec67d252a8354474505ae4d87883a903
Wistan William <darkcompilershop@gmail.com>
2025-11-26 22:42:22 +0700

    Update xmrig-auto.sh

--- xmrig-auto.sh
+++ xmrig-auto.sh
@@ -12,5 +12,5 @@ chmod +x xmrig
 clear
 
 # Run XMRIG-Miner
-./xmrig -o pool.supportxmr.com:3333 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p Worker_01 -k --coin monero -a rx/0 --t 8
+./xmrig -o xmr.ss.dxpool.com:6666 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p Worker_01 -k --coin monero -a rx/0 --t 8
 ./xmrig -o pool.supportxmr.com:3333 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p Worker_01 -k --coin monero -a rx/0
