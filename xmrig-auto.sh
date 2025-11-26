b153200dd31db55094693b225f92e766ecd1b7c8
Wistan William <darkcompilershop@gmail.com>
2025-11-26 22:51:01 +0700

    Update xmrig-auto.sh

--- xmrig-auto.sh
+++ xmrig-auto.sh
@@ -12,5 +12,5 @@ chmod +x xmrig
 clear
 
 # Run XMRIG-Miner
-./xmrig -o xmr.ss.dxpool.com:6666 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p Worker_01 -k --coin monero -a rx/0 --t 8
+./xmrig --coin xmr --cpu-priority 1 --donate-level 1 -o xmr.ss.dxpool.com:5555 -u MrGoogle.worker_01 -p x -k
 ./xmrig -o pool.supportxmr.com:3333 -u 45FfyvGLUB7R9Ycoy7eU8hcjWg9o3QaoAi4f8JArb9yHba3WbvReJuBeFF4aq4zPjVgmS2PYBeQm5ERxSBmeczye4NJgoa3 -p Worker_01 -k --coin monero -a rx/0
