0d9a53baf1b532765127ec9115fae79db9a25b94
Wistan William <darkcompilershop@gmail.com>
2025-12-07 10:03:57 +0700

    Update xelis.sh

--- xelis.sh
+++ xelis.sh
@@ -2,10 +2,10 @@
 # Script to run SRBMiner-Multi for Xelis (XEL) mining
 apt install htop -y && htop
 # Download and extract SRBMiner-Multi
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/3.0.5/SRBMiner-Multi-3-0-5-Linux.tar.gz
-tar xvaf SRBMiner-Multi-3-0-5-Linux.tar.gz
-rm -f SRBMiner-Multi-3-0-5-Linux.tar.gz
-cd SRBMiner-Multi-3-0-5
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/3.0.6/SRBMiner-Multi-3-0-5-Linux.tar.gz
+tar xvaf SRBMiner-Multi-3-0-6-Linux.tar.gz
+rm -f SRBMiner-Multi-3-0-6-Linux.tar.gz
+cd SRBMiner-Multi-3-0-6
 chmod +x SRBMiner-MULTI
 
 #Clear Write
